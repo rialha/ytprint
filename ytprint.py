92ba72bda7d8d2b2db7c3a6a2c9428169313d07f
rialha <40536768+rialha@users.noreply.github.com>
2020-05-13 16:47:26 +0200

    Update ytprint.py

--- ytprint.py
+++ ytprint.py
@@ -2,6 +2,7 @@ from pytube import YouTube
 import pysubs2
 import os
 
+# Fetch the YouTube captions in a format the program can read
 subs = YouTube(input("YouTube video URL: ")).captions.get_by_language_code('en').generate_srt_captions()
 
 f = open("subs.srt", "w+")
@@ -9,8 +10,9 @@ f.write(subs)
 f.close()
 subs = pysubs2.load("subs.srt")
 os.remove("subs.srt")
-
 text = ""
+
+# Add every line to be printed, and print it out at the end
 for line in subs:
   text+=(line.text+"\n")
 print(text)
